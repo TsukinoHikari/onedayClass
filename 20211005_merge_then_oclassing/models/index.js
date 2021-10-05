931f0cf498315c44ced524307d831a6318a9de10
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-06 01:20:35 +0900

    20211006_merge_then_classing
    메인 머지 후 클래스 등록 작업중

---
+++ 20211005_merge_then_oclassing/models/index.js
@@ -0,0 +1,66 @@
+const Sequelize = require("sequelize");
+const User = require("./user");
+const Oclass = require("./oclass");
+const Admin = require("./admin");
+const Notice = require("./notice");
+const Auth = require("./emailauth");
+
+const bcrypt = require("bcrypt");
+
+const a = () => {
+    const password = 1234;
+    const hash = bcrypt.hash(password, 12);
+    Admin.create({
+        adminId: "admin",
+        adminPwd: hash,
+        adminName: "비대면수업",
+        adminTel: "010-1234-4567",
+        adminMail: "gg@naver.com",
+    });
+};
+a;
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
+const db = {};
+
+const sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config,
+
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASS,
+    {
+        host: process.env.MYSQL_URL,
+        dialect: "mysql",
+        timezone: "+09:00", // DB에 저장할 때 시간 설정
+        dialectOptions: {
+            timezone: "+09:00", // DB에서 가져올 때 시간 설정
+        },
+    }
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Oclass = Oclass;
+db.Admin = Admin;
+db.Notice = Notice;
+db.Auth = Auth;
+
+User.init(sequelize);
+Oclass.init(sequelize);
+Admin.init(sequelize);
+Notice.init(sequelize);
+Auth.init(sequelize);
+
+User.associate(db);
+Oclass.associate(db);
+Admin.associate(db);
+Notice.associate(db);
+Auth.associate(db);
+
+module.exports = db;
