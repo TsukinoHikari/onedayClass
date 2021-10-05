8a54176807080f1630ede952580ca07585fa5103
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-05 14:36:21 +0900

    asdf

--- models/index.js
+++ models/index.js
@@ -6,15 +6,15 @@ const Notice = require("./notice");
 const bcrypt = require("bcrypt");
 
 const a = () => {
-  const password = 1234;
-  const hash = bcrypt.hash(password, 12);
-  Admin.create({
-    adminId: "admin",
-    adminPwd: hash,
-    adminName: "비대면수업",
-    adminTel: "010-1234-4567",
-    adminMail: "gg@naver.com",
-  });
+    const password = 1234;
+    const hash = bcrypt.hash(password, 12);
+    Admin.create({
+        adminId: "admin",
+        adminPwd: hash,
+        adminName: "비대면수업",
+        adminTel: "010-1234-4567",
+        adminMail: "gg@naver.com",
+    });
 };
 a;
 const env = process.env.NODE_ENV || "development";
@@ -22,22 +22,22 @@ const config = require("../config/config")[env];
 const db = {};
 
 const sequelize = new Sequelize(
-  config.database,
-  config.username,
-  config.password,
-  config,
+    config.database,
+    config.username,
+    config.password,
+    config,
 
-  process.env.MYSQL_DB,
-  process.env.MYSQL_USER,
-  process.env.MYSQL_PASS,
-  {
-    host: process.env.MYSQL_URL,
-    dialect: "mysql",
-    timezone: "+09:00", // DB에 저장할 때 시간 설정
-    dialectOptions: {
-      timezone: "+09:00", // DB에서 가져올 때 시간 설정
-    },
-  }
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASS,
+    {
+        host: process.env.MYSQL_URL,
+        dialect: "mysql",
+        timezone: "+09:00", // DB에 저장할 때 시간 설정
+        dialectOptions: {
+            timezone: "+09:00", // DB에서 가져올 때 시간 설정
+        },
+    }
 );
 
 db.sequelize = sequelize;
