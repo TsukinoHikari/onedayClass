b51a56728683deff7c77ee1b7a4db97867bfb7bc
pier101 <achievedw@gmail.com>
2021-10-09 15:00:34 +0900

    211009_wishlist추가
    ㄴㄴ

--- models/index.js
+++ models/index.js
@@ -1,22 +1,102 @@
-const Sequelize = require('sequelize');
-const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config')[env];
-const User = require('./user');
+const Sequelize = require("sequelize");
+const User = require("./user");
+const Oclass = require("./oclass");
+const Admin = require("./admin");
+const Notice = require("./notice");
+const Auth = require("./emailauth");
+const UrlPath = require("./urlPath");
+const WishList = require("./wishlist");
 
+const bcrypt = require("bcrypt");
 
+const a = () => {
+    const password = 1234;
+    const hash = bcrypt.hash(password, 12);
+    Admin.create({
+        adminId: "admin",
+        adminPwd: hash,
+        adminName: "비대면수업",
+        adminTel: "010-1234-4567",
+        adminMail: "gg@naver.com",
+    });
+};
+a;
+
+const env = process.env.NODE_ENV || "development";
+const config = require("../config/config")[env];
 const db = {};
+
 const sequelize = new Sequelize(
-  config.database, config.username, config.password, config,
+    config.database,
+    config.username,
+    config.password,
+    config,
+
+    process.env.MYSQL_DB,
+    process.env.MYSQL_USER,
+    process.env.MYSQL_PASS,
+    {
+        host: process.env.MYSQL_URL,
+        dialect: "mysql",
+        timezone: "+09:00", // DB에 저장할 때 시간 설정
+        dialectOptions: {
+            timezone: "+09:00", // DB에서 가져올 때 시간 설정
+        },
+    }
 );
 
 db.sequelize = sequelize;
-db.User = User;
+db.Sequelize = Sequelize;
 
+db.User = User;
+db.Oclass = Oclass;
+db.Admin = Admin;
+db.Notice = Notice;
+db.Auth = Auth;
+db.UrlPath = UrlPath;
+db.WishList = WishList;
+
+// db.images = require("./image.model.js")(sequelize, Sequelize);
 
 User.init(sequelize);
-
+Oclass.init(sequelize);
+Admin.init(sequelize);
+Notice.init(sequelize);
+Auth.init(sequelize);
+UrlPath.init(sequelize);
+WishList.init(sequelize);
 
 User.associate(db);
+Oclass.associate(db);
+Admin.associate(db);
+Notice.associate(db);
+Auth.associate(db);
+UrlPath.associate(db);
+WishList.associate(db);
 
+const oClassPath = sequelize.define(
+    "oClassPath",
+    {
+        oClassPathNum: {
+            type: Sequelize.INTEGER.UNSIGNED,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "oClassPath",
+        tableName: "oClassPaths",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+    }
+);
+
+Oclass.belongsToMany(UrlPath, { through: oClassPath });
+UrlPath.belongsToMany(Oclass, { through: oClassPath });
 
 module.exports = db;
--- routes/page.js
+++ routes/page.js
@@ -1,12 +1,33 @@
-const express = require('express');
+const express = require("express");
+const { Auth } = require("../models");
+const { isNotLoggedIn, isLoggedIn } = require("./middlewares");
 
 const router = express.Router();
 
-router.get('/',(req,res)=>{
-    res.render('main');
-})
-router.get('/auth',(req,res)=>{
-    res.render('join');
-})
+router.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
+router.get("/login", isNotLoggedIn, (req, res) => {
+    res.render("login", { title: "로그인" });
+});
+router.get("/findidpwd", isNotLoggedIn, (req, res) => {
+    res.render("findidpwd", { title: "아이디/비밀번호 찾기" });
+});
+router.get("/resetidpw/:token", isNotLoggedIn, async (req, res) => {
+    res.render("reset-password", { title: "비밀번호 재설정" });
+});
+router.get("/mypage", isLoggedIn, (req, res) => {
+    res.render("mypage", { title: "내정보" });
+});
+router.get("/join", isNotLoggedIn, (req, res) => {
+    res.render("join", { title: "회원가입" });
+});
+router.get("/", (req, res) => {
+    res.render("main", {
+        title: "원데이클래스",
+    });
+});
 
 module.exports = router;
