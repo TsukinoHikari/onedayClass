4fb60b6f2c06c0493331261465864dd306c30b06
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-14 09:57:58 +0900

    2021-10-14

---
+++ 20211014_(내메인부터)10시부터/passport/index.js
@@ -0,0 +1,31 @@
+const passport = require("passport");
+const localu = require("./localStrategyu");
+const locala = require("./localStrategya");
+
+const kakao = require("./kakaoStrategy");
+const User = require("../models/user");
+const Admin = require("../models/admin");
+
+module.exports = () => {
+    passport.serializeUser((user, done) => {
+        done(null, user.userId);
+    });
+    passport.deserializeUser((userId, done) => {
+        User.findOne({ where: { userId } })
+            .then((user) => done(null, user))
+            .catch((err) => done(err));
+    });
+
+    // passport.serializeUser((admin, done) => {
+    //     done(null, admin.adminId);
+    // });
+    // passport.deserializeUser((adminId, done) => {
+    //     Admin.findOne({ where: { adminId } })
+    //         .then((admin) => done(null, admin))
+    //         .catch((err) => done(err));
+    // });
+
+    localu();
+    //locala();
+    kakao();
+};
