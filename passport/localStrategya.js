66fe5e6aaab3db448d2224a6f93df14413286819
TsukinoHikari <89628687+TsukinoHikari@users.noreply.github.com>
2021-10-14 19:02:17 +0900

    CSS merge
    메인 페이지 하던 도중 입니다. 마저 연결해서 할 예정이니 메인 이외의 부분만 만져주시면 될 것 같습니다.

--- passport/localStrategya.js
+++ passport/localStrategya.js
@@ -6,7 +6,6 @@ const Admin = require("../models/admin");
 
 //const User = require('../models/user');
 
-// 이름 상의 요망.
 module.exports = () => {
     passport.use(
         new LocalStrategy(
@@ -43,29 +42,4 @@ module.exports = () => {
     );
 };
 
-/*  이름 상의 요망.
- module.exports = ()=>{
-   passport.use(new LocalStrategy({    
-       //userid는 안되고 usermail은 왜 될까?
-       usernameField: 'userid',
-       passwordField: 'userpwd',
-   },async (userid,userpwd,done)=>{
-       try{
-           const exUser = await User.findOne({where: {userid}});
-           if(exUser){
-               const result = await bcrypt.compare(userpwd,exUser.userpwd);
-               if (result) {
-                   done(null,exUser);
-               }else{
-                   done(null,false,{message:'비밀번호가 일치하지 않습니다.'})
-               }
-           } else{
-               done(null,false,{message:'가입되지 않은 회원입니다.'});
-           }
-       } catch(error){
-           console.error(error);
-           done(error);
-       }
-   }));
- };
-*/
+
