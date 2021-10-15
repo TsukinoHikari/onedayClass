ad562047f747c15d75adb6855d3efb5b6cb9b088
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-15 09:27:38 +0900

    chs_search_img_9am27

--- routes/search.js
+++ routes/search.js
@@ -38,37 +38,29 @@ router.get("/", async (req, res) => {
         });
         let classImages = [];
         for (let i = 0; i < searchClass.length; i++) {
-            let sql = `SELECT urlpaths.path, oclasses.classTitle FROM oclasspaths INNER JOIN urlpaths ON oclasspaths.UrlPathId = urlpaths.id INNER JOIN oclasses ON oclasses.classNum=oclasspaths.OclassClassNum WHERE OclassClassNum=${searchClass[i].classNum} GROUP BY OclassClassNum;`;
+            let sql = `SELECT oclasses.classNum, urlpaths.path, oclasses.classTitle FROM oclasspaths INNER JOIN urlpaths ON oclasspaths.UrlPathId = urlpaths.id INNER JOIN oclasses ON oclasses.classNum=oclasspaths.OclassClassNum WHERE OclassClassNum=${searchClass[i].classNum} GROUP BY OclassClassNum;`;
             let classImage = await sequelize.query(sql, {
                 type: QueryTypes.SELECT,
             });
             classImages.push(classImage);
         }
-        console.log(classImages);
 
         let mapTheList = function (list) {
             // list 형식이야 어쨌든 전체를 순회하면서
             return list.map(function (item) {
-                // 하나의 객체로 만들기로 한다.
-                let obj = {};
-                // 모두 길이가 2인 배열들로 이루어져 있다고 간주하고 전체를 돌면서 객체에 바로 넣고
                 for (let i = 0; i < item.length; i++) {
                     let attr = item[i];
-                    obj[attr[0]] = attr[1];
-                    obj[attr[1]] = attr[2];
-                    obj[attr[2]] = attr[3];
+                    return attr;
                 }
 
                 // 반환한다.
-                return obj;
             });
         };
 
-        // 객체들의 배열(array of objects)은 정렬, 필터링 등의 방법이 이미 인터넷에 많이 있습니다.
-        console.log(mapTheList(classImages));
+        // // 객체들의 배열(array of objects)은 정렬, 필터링 등의 방법이 이미 인터넷에 많이 있습니다.
+        const searchClasses = mapTheList(classImages);
 
-        res.json(classImages);
-        //return res.json("search", { classImages });
+        return res.render("search", { searchClasses });
     } catch (err) {
         console.error(err);
         next(err);
