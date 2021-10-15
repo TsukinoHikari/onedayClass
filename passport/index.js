db2469dc263b1432d9efbffaa1f59723a892cf5a
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2021-10-15 09:55:34 +0900

    little..

--- passport/index.js
+++ passport/index.js
@@ -8,25 +8,25 @@ const Admin = require("../models/admin");
 const googleStrategy = require("./googleStrategy");
 
 module.exports = () => {
-    // passport.serializeUser((user, done) => {
-    //     done(null, user.userId);
-    // });
-    // passport.deserializeUser((userId, done) => {
-    //     User.findOne({ where: { userId } })
-    //         .then((user) => done(null, user))
-    //         .catch((err) => done(err));
-    // });
-    // localu();
-
-    passport.serializeUser((admin, done) => {
-        done(null, admin.adminId);
+    passport.serializeUser((user, done) => {
+        done(null, user.userId);
     });
-    passport.deserializeUser((adminId, done) => {
-        Admin.findOne({ where: { adminId } })
-            .then((admin) => done(null, admin))
+    passport.deserializeUser((userId, done) => {
+        User.findOne({ where: { userId } })
+            .then((user) => done(null, user))
             .catch((err) => done(err));
     });
-    locala();
+    localu();
+
+    // passport.serializeUser((admin, done) => {
+    //     done(null, admin.adminId);
+    // });
+    // passport.deserializeUser((adminId, done) => {
+    //     Admin.findOne({ where: { adminId } })
+    //         .then((admin) => done(null, admin))
+    //         .catch((err) => done(err));
+    // });
+    // locala();
 
     kakao();
     googleStrategy();
